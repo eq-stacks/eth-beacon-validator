fd318a5f8a1958c91997d48c809c8ef996ff3b23
Mark Henderson <mark@mrh.io>
2022-01-21 20:32:01 -0500

    refactor: only use latest snapshot

--- snapshots/create-volume-snapshots.sh
+++ snapshots/create-volume-snapshots.sh
@@ -8,11 +8,7 @@ kubectl -n validators get persistentvolumeclaims --no-headers -o=custom-columns=
 
     PVC_NAME=$(echo "$line")
     SNAPSHOT_NAME=$(echo "$line" | cut -d "-" -f 5,6,7)
-    SNAPSHOT_TIME=$(date +%s)
 
-    envsubst < ./snapshots/volumesnapshot.template.yml | kubectl apply -f -    
-
-    SNAPSHOT_TIME=latest
     kubectl delete volumesnapshot ${SNAPSHOT_NAME}-snapshot-latest 
     envsubst < ./snapshots/volumesnapshot.template.yml | kubectl apply -f -    
 done
