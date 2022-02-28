9c304533e2231b8d1da9b34701d46be7f18999b8
Mark Henderson <mark@mrh.io>
2022-02-28 15:38:01 -0500

    fix: terraform fmt

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,7 +17,7 @@ terraform {
 
 provider "google" {
   credentials = file(var.credentials_file)
-  project = var.service_account_project
+  project     = var.service_account_project
 }
 
 data "google_service_account" "terraform" {
@@ -25,17 +25,17 @@ data "google_service_account" "terraform" {
 }
 
 resource "google_project" "eth2_validator_project" {
-  name       = "Eth2 Consensus Validator"
-  project_id = var.project_eth_validator
-  org_id     = var.org_id
+  name                = "Eth2 Consensus Validator"
+  project_id          = var.project_eth_validator
+  org_id              = var.org_id
   auto_create_network = false
 
   billing_account = var.billing_account
 }
 
 resource "google_project_iam_binding" "eth_validator_project" {
-  project     = var.project_eth_validator
-  role = "roles/owner"
+  project = var.project_eth_validator
+  role    = "roles/owner"
   members = var.project_owners
   depends_on = [
     google_project.eth2_validator_project
@@ -59,17 +59,17 @@ resource "google_project_service" "container" {
 
 resource "google_project_iam_binding" "host_project" {
   project = var.project_host_vpc
-  role = "roles/container.hostServiceAgentUser"
+  role    = "roles/container.hostServiceAgentUser"
   members = [
     "serviceAccount:service-${google_project.eth2_validator_project.number}@container-engine-robot.iam.gserviceaccount.com"
   ]
 }
 
 resource "google_compute_subnetwork_iam_binding" "subnetwork" {
-  project = var.project_host_vpc
-  region = "us-east4"
+  project    = var.project_host_vpc
+  region     = "us-east4"
   subnetwork = "us-east4"
-  role = "roles/compute.networkUser"
+  role       = "roles/compute.networkUser"
   members = [
     "serviceAccount:service-${google_project.eth2_validator_project.number}@container-engine-robot.iam.gserviceaccount.com",
     "serviceAccount:${google_project.eth2_validator_project.number}@cloudservices.gserviceaccount.com"
@@ -77,31 +77,31 @@ resource "google_compute_subnetwork_iam_binding" "subnetwork" {
 }
 
 data "google_compute_network" "host_vpc" {
-  name = "host-vpc"
+  name    = "host-vpc"
   project = var.project_host_vpc
 }
 
 data "google_compute_subnetwork" "us_east4" {
-  name   = "us-east4-sn1"
-  region = "us-east4"
+  name    = "us-east4-sn1"
+  region  = "us-east4"
   project = var.project_host_vpc
 }
 
 resource "google_container_cluster" "testnet" {
   name     = "testnet-cluster"
   location = "us-east4-c"
-  project = google_project.eth2_validator_project.project_id
+  project  = google_project.eth2_validator_project.project_id
 
   # Create the smallest possible default node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  network = data.google_compute_network.host_vpc.self_link
+  network    = data.google_compute_network.host_vpc.self_link
   subnetwork = "projects/host-vpc-project-10/regions/us-east4/subnetworks/us-east4"
   # network = "projects/host-vpc-project-10/global/networks/host-vpc"
 
   ip_allocation_policy {
-    cluster_secondary_range_name = "us-east4-pods"
+    cluster_secondary_range_name  = "us-east4-pods"
     services_secondary_range_name = "us-east4-services"
   }
 
